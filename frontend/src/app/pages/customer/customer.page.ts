f4294fd58de7844c513ec2096ff4b22b51564a26
magantus@googlemail.com <Extrem93!>
2021-12-23 00:25:39 +0100

    update

--- frontend/src/app/pages/customer/customer.page.ts
+++ frontend/src/app/pages/customer/customer.page.ts
@@ -14,41 +14,59 @@ export class CustomerPage implements OnInit {
   customer: Customer[] = [];
   newCustomer: Customer;
   processCustomer: Customer;
+
   constructor(private customerService: CustomerService) {
   }
 
   ngOnInit() {
     this.processCustomer = new Customer();
-    this.newCustomer=new Customer();
+    this.newCustomer = new Customer();
+    this.loadAllCustomer();
+  }
+
+  loadAllCustomer() {
+    this.customer = [];
     this.customerService.getAllCustomer().forEach(value => {
       value.forEach(customer => {
         this.customer.push(customer);
       });
     });
   }
-  resetNewCustomer(){
-    this.newCustomer= new Customer();
+
+  resetNewCustomer() {
+    this.newCustomer = new Customer();
   }
-  deleteProcessCustomer(){
-   this.customerService.deleteCustomer(this.processCustomer.email);
-   this.showMember=false;
-   this.processCustomer=new Customer();
+
+  deleteProcessCustomer() {
+    this.customerService.deleteCustomer(this.processCustomer.email);
+    this.showMember = false;
+    this.customer.forEach((customer, index) => {
+      if (customer === this.processCustomer) {
+        this.customer.splice(index, 1);
+      }
+    });
+    this.processCustomer = new Customer();
   }
-  saveProcessCustomer(){
+
+  saveProcessCustomer() {
     this.customerService.saveCustomer(this.processCustomer);
   }
+
   saveCustomer() {
     this.customerService.saveCustomer(this.newCustomer);
     this.newCustomer = new Customer();
+    this.customer.push(this.newCustomer);
+  }
 
+  loadCustomerProcess(customer: Customer) {
+    this.showMember = true;
+    this.processCustomer = customer;
   }
-  loadCustomerProcess(customer: Customer){
-    this.showMember=true;
-    this.processCustomer=customer;
-  }
+
   changeNewMemberView() {
     this.showNewMember = !this.showNewMember;
   }
+
   changeMemberView() {
     this.showMember = !this.showMember;
   }
