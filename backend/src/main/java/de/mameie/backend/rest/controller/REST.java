22bf9e758974e28708024b988f1c1e93d3a0a13a
magantus@googlemail.com <Extrem93!>
2022-01-06 00:20:39 +0100

    update

--- backend/src/main/java/de/mameie/backend/rest/controller/REST.java
+++ backend/src/main/java/de/mameie/backend/rest/controller/REST.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public interface REST<T> {
 
-    void delete(Long id);
+    void delete(Long id) throws IOException;
     T get(Long id) throws IOException;
     List<T> getAll() throws IOException;
-    void put(T object);
+    void put(T object) throws IOException;
     void post(T object) throws IOException;
 }
