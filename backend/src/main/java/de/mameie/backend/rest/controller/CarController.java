5baf420b8392db87939bebf950f4e2b3d56751f3
magantus@googlemail.com <Extrem93!>
2022-02-05 02:23:05 +0100

    update

--- backend/src/main/java/de/mameie/backend/rest/controller/CarController.java
+++ backend/src/main/java/de/mameie/backend/rest/controller/CarController.java
@@ -2,10 +2,12 @@ package de.mameie.backend.rest.controller;
 
 
 import de.mameie.backend.rest.model.dto.BrandDto;
+import de.mameie.backend.rest.model.dto.CarTypeDto;
 import de.mameie.backend.rest.model.entity.BrandEntity;
+import de.mameie.backend.rest.model.entity.CarTypeEntity;
 import de.mameie.backend.rest.service.BrandService;
+import de.mameie.backend.rest.service.CarTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -17,10 +19,12 @@ import java.util.List;
 public class CarController {
 
     private final BrandService brandService;
+    private final CarTypeService carTypeService;
 
     @Autowired
-    public CarController(BrandService brandService) {
+    public CarController(BrandService brandService, CarTypeService carTypeService) {
         this.brandService = brandService;
+        this.carTypeService = carTypeService;
     }
 
 
@@ -36,6 +40,19 @@ public class CarController {
             brandDtos.add(brandService.convertEntityToDto(brandEntity));
         });
         return brandDtos;
+    }
+    @PostMapping(value = "/carType/{type}")
+    public void saveCarType(@PathVariable("type") String type) throws IOException {
+        carTypeService.post(new CarTypeEntity(type));
+    }
+
+    @GetMapping(value = "/carType")
+    public List<CarTypeDto> loadCarType() throws IOException {
+        List<CarTypeDto> carTypeDtos = new ArrayList<>();
+        this.carTypeService.getAll().stream().forEach(carTypEntity -> {
+            carTypeDtos.add(carTypeService.convertEntityToDto(carTypEntity));
+        });
+        return carTypeDtos;
     }
 
 
--- frontend/src/app/pages/car/car-create/car-create.page.ts
+++ frontend/src/app/pages/car/car-create/car-create.page.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Car} from '../../../models/Car';
+import {CarService} from '../../../services/http/car.service';
+import {CarType} from '../../../models/CarType';
+import {Brand} from '../../../models/Brand';
+import {Customer} from '../../../models/Customer';
 
 @Component({
   selector: 'app-car-create',
@@ -7,29 +11,63 @@ import {Car} from '../../../models/Car';
   styleUrls: ['./car-create.page.scss'],
 })
 export class CarCreatePage implements OnInit {
-
+  path: string;
   car: Car;
-  constructor() {
+  carTypes: CarType[] = [];
+  brands: Brand[] = [];
+  fuels: string[] = [];
+  gears: string[] = [];
+  customers: Customer[] = [];
+
+  constructor(private carService: CarService) {
   }
 
 
   ngOnInit() {
     this.car = new Car();
+    this.initData();
   }
 
+
   saveCar() {
-    return null;
+    this.carService.saveCar(this.car);
   }
 
   saveFile() {
+    if (this.path.length === 0 || this.path == null) {
+      alert('Bitte Pfad eingeben');
+      return;
+    }
     return null;
   }
 
-  openFileLoader() {
-    return null;
-  }
 
   refreshCar() {
-    return null;
+    this.car = new Car();
+  }
+
+  initData() {
+    this.carService.getAllCarTypes().forEach(carTypeList => {
+      carTypeList.forEach(carType => {
+        this.carTypes.push(carType);
+      });
+    });
+    this.carService.getAllBrands().forEach(brandList => {
+      brandList.forEach(brand => {
+        this.brands.push(brand);
+      });
+    });
+    this.carService.getAllCustomer().forEach(customerList => {
+      customerList.forEach(customer => {
+        this.customers.push(customer);
+      });
+    });
+    this.fuels.push('Benzin');
+    this.fuels.push('Elektro');
+    this.fuels.push('Diesel');
+
+    this.gears.push('Schaltgetriebe');
+    this.gears.push('Automatikgetriebe');
+    this.path='';
   }
 }
